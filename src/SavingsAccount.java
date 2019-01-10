435f34e9de5cdbf8bfe199430661689a65e84b04
jh1923 <42978576+jh1923@users.noreply.github.com>
2019-01-10 13:57:44 -0500

    Update SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,9 @@
-
+/**
+ * 
+ * @author Julia Hu
+ * SavingsAccount Class
+ *
+ */
 public class SavingsAccount extends BankAccount
 {
 	//fields
@@ -7,8 +12,14 @@ public class SavingsAccount extends BankAccount
 	private final double MIN_BAL_FEE;
 	
 	//constructors
-	
-	//initial balance determined when constructor is called
+	/**
+	 * creates SavingsAccount with a given balance
+	 * @param n name
+	 * @param b balance
+	 * @param r rate
+	 * @param mb minimum balance
+	 * @param mbf minimum balance fee
+	 */
 	public SavingsAccount(String n, double b, double r, double mb, double mbf)
 	{
 		super(n, b);
@@ -17,13 +28,23 @@ public class SavingsAccount extends BankAccount
 		MIN_BAL_FEE = mbf;
 	}
 	
-	//initial balance of zero
+	/**
+	 * creates SavingsAccount where initial balance is zero
+	 * @param n name
+	 * @param r rate
+	 * @param mb minimum balance
+	 * @param mbf minimum balance fee
+	 */
 	public SavingsAccount(String n, double r, double mb, double mbf)
 	{
 		this(n, 0, r, mb, mbf);
 	}
 	
 	//methods
+	/**
+	 * withdraws money from SavingsAccount
+	 * @param amt the amount of money to be withdrawn
+	 */
 	public void withdraw(double amt)
 	{
 		if (getBalance() - amt < 0)
@@ -40,6 +61,11 @@ public class SavingsAccount extends BankAccount
 			}
 		}
 	}
+	
+	/**
+	 * deposits money into SavingsAccount
+	 * @param amt the amount of money to be deposited
+	 */
 	public void deposit(double amt)
 	{
 		if (amt < 0)
@@ -48,6 +74,11 @@ public class SavingsAccount extends BankAccount
 			super.deposit(amt);
 	}	
 	
+	/**
+	 * transfers money from one account to another
+	 * @param other BankAccount that is being deposited into
+	 * @param amt amount of money being transferred from SavingsAccount
+	 */
 	public void transfer(BankAccount other, double amt)
 	{
 		if (!(other.getName().equals(getName())))
@@ -58,11 +89,17 @@ public class SavingsAccount extends BankAccount
 			super.transfer(other,amt);
 	}
 	
+	/**
+	 * calculates and adds interest to SavingsAccount
+	 */
 	public void addInterest()
 	{
 		deposit(getBalance() + getBalance()*intRate);
 	}
 	
+	/**
+	 * updates at end of month by adding interest to account
+	 */
 	public void endOfMonthUpdate()
 	{
 		addInterest();
